e656efd1b36f014454bf997f9ee13308217b6670
francistobi <frobigwe305@gmail.com>
2024-03-17 11:43:43 +0100

    new commit

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const express = require("express");
 const tasks = require("./routes/tasks");
 const { connectToMongodb } = require("./db/connect");
 const notFound = require("./middleware/not-found");
-const errorMiddleware = require("./middleware/error-handler");
+// const errorMiddleware = require("./middleware/error-handler");
 const url = process.env.connection_string;
 
-const app = express()
+const app = express();
 
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //routes
-app.use("/api/v1/tasks", tasks)
+app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
 // app.use(errorMiddleware)
@@ -27,7 +27,7 @@ const start = async () => {
       console.log(`Server started successfully at port ${port}...`);
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
